0036e9bac45328972b1b33e61e1df9edcf58fbf8
Jokan <1012233094@qq.com>
2020-09-02 00:13:48 +0900

    Google map commit

--- lib/google_map_page.dart
+++ lib/google_map_page.dart
@@ -1,10 +1,42 @@
 import 'dart:async';
+// import 'dart:html';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:search_map_place/search_map_place.dart';
 // import 'package:location/location.dart' as LocationManager;
+
+// class LocationData {
+//   final double latitude; // Latitude, in degrees
+//   final double longitude; // Longitude, in degrees
+//   final double accuracy; // Estimated horizontal accuracy of this location, radial, in meters
+//   final double altitude; // In meters above the WGS 84 reference ellipsoid
+//   final double speed; // In meters/second
+//   final double speedAccuracy; // In meters/second, always 0 on iOS
+//   final double heading; //Heading is the horizontal direction of travel of this device, in degrees
+//   final double time; //timestamp of the LocationData
+// }
+//
+//
+// enum LocationAccuracy {
+//   powerSave, // To request best accuracy possible with zero additional power consumption,
+//   low, // To request "city" level accuracy
+//   balanced, // To request "block" level accuracy
+//   high, // To request the most accurate locations available
+//   navigation // To request location for navigation usage (affect only iOS)
+// }
+//
+// // Status of a permission request to use location services.
+// enum PermissionStatus {
+//   /// The permission to use location services has been granted.
+//   granted,
+//   // The permission to use location services has been denied by the user. May have been denied forever on iOS.
+//   denied,
+//   // The permission to use location services has been denied forever by the user. No dialog will be displayed on permission request.
+//   deniedForever
+// }
+
 
 class GoogleMapPage extends StatefulWidget {
   @override
@@ -14,19 +46,29 @@ class GoogleMapPage extends StatefulWidget {
 class _GoogleMapPageState extends State<GoogleMapPage> {
   // GoogleMapController mapController;
   final homeScaffoldKey = GlobalKey<ScaffoldState>();
-  Completer<GoogleMapController> _controller = Completer();
+  final Map<String, Marker> _markers = {};
+  GoogleMapController mapController;
 
   static final CameraPosition _myLocation = CameraPosition(
     target: LatLng(35.6580339, 139.7016358),
     zoom: 17.0,
   );
 
-  LatLng _initialPosition = LatLng(37.42796133588664, -122.885740655967);
-  Set<Marker> markers = Set(
+  LatLng markerPosition = LatLng(35.6580339, 139.7016358);
 
-  );
   void _onMapCreated(GoogleMapController controller) {
-    _controller.complete(controller);
+    setState(() {
+      mapController = controller;
+      // var options = MarkerOptions(
+      //     position: LatLng(35.6580339,139.7016358)
+      // );
+      var marker = Marker(
+          markerId: MarkerId("1"),
+          position: markerPosition,
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRose),
+          infoWindow: InfoWindow(title: "テストタイトル", snippet: "テスト内容"));
+      _markers["テストタイトル"] = marker;
+    });
   }
 
   @override
@@ -37,39 +79,53 @@ class _GoogleMapPageState extends State<GoogleMapPage> {
           title: Text("Test Of Map App"),
           backgroundColor: Colors.cyan,
         ),
-        body:Stack(
+        body: Stack(
           children: <Widget>[
             Container(
-              child:
-              GoogleMap(
+              child: GoogleMap(
                 onMapCreated: _onMapCreated,
                 initialCameraPosition: _myLocation,
-                markers: markers,
+                markers: _markers.values.toSet(),
+                onLongPress: (LatLng latLng) {
+                  // you have latitude and longitude here
+                  setState(() {
+                    _markers["テストタイトル"] = Marker(
+                        markerId: MarkerId("1"),
+                        position: latLng,
+                        icon: BitmapDescriptor.defaultMarkerWithHue(
+                            BitmapDescriptor.hueRose),
+                        infoWindow:
+                            InfoWindow(title: "テストタイトル", snippet: "テスト内容"));
+                  });
+                  print("测试测试测试测试");
+                  print(markerPosition.latitude);
+                  print(markerPosition.longitude);
+                },
                 myLocationEnabled: true,
                 myLocationButtonEnabled: true,
               ),
             ),
-            Container(
-              child: SearchMapPlaceWidget(
-                apiKey: "AIzaSyAPyQctQI4I_hKXicBWQJy1OZsQj0NR2pQ",
-                // The language of the autocompletion
-                language: 'en',
-                // The position used to give better recomendations. In this case we are using the user position
-                // location: userPosition.coordinates,
-                // radius: 30000,
-                onSelected: (Place place) async {
-                  final geolocation = await place.geolocation;
-
-                  // Will animate the GoogleMap camera, taking us to the selected position with an appropriate zoom
-                  final GoogleMapController controller =
-                      await _controller.future;
-                  controller.animateCamera(
-                      CameraUpdate.newLatLng(geolocation.coordinates));
-                  controller.animateCamera(
-                      CameraUpdate.newLatLngBounds(geolocation.bounds, 0));
-                },
-              ),
-            )
+            // Container(
+            //   child: SearchMapPlaceWidget(
+            //     apiKey: "AIzaSyAPyQctQI4I_hKXicBWQJy1OZsQj0NR2pQ",
+            //     // The language of the autocompletion
+            //     language: 'en',
+            //     // The position used to give better recomendations. In this case we are using the user position
+            //     // location: userPosition.coordinates,
+            //     // radius: 30000,
+            //     onSelected: (Place place) async {
+            //       final geolocation = await place.geolocation;
+            //
+            //       // Will animate the GoogleMap camera, taking us to the selected position with an appropriate zoom
+            //       final GoogleMapController controller =
+            //           await mapController;
+            //       controller.animateCamera(
+            //           CameraUpdate.newLatLng(geolocation.coordinates));
+            //       controller.animateCamera(
+            //           CameraUpdate.newLatLngBounds(geolocation.bounds, 0));
+            //     },
+            //   ),
+            // )
           ],
         ));
   }
